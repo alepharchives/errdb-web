30bcaa68c9eea94303b0fa78593f484c6571cc5c
Ery Lee <ery.lee@gmail.com>
2012-02-22 15:32:01 +0800

    first workable release

--- src/errdb_web.erl
+++ src/errdb_web.erl
@@ -5,7 +5,7 @@
 
 -module(errdb_web).
 -author('author <author@example.com>').
--export([start/0, stop/0]).
+-export([start/0, stop/0, errdb_node/0]).
 
 ensure_started(App) ->
     case application:start(App) of
@@ -18,9 +18,15 @@ ensure_started(App) ->
 %% @spec start() -> ok
 %% @doc Start the errdb_web server.
 start() ->
+	init_elog(),
     errdb_web_deps:ensure(),
     ensure_started(crypto),
     application:start(errdb_web).
+
+init_elog() ->
+    {ok, [[LogLevel]]} = init:get_argument(log_level),
+    {ok, [[LogPath]]} = init:get_argument(log_path),
+	elog:init(list_to_integer(LogLevel), LogPath).
 
 %% @spec stop() -> ok
 %% @doc Stop the errdb_web server.
@@ -28,3 +34,15 @@ stop() ->
     Res = application:stop(errdb_web),
     application:stop(crypto),
     Res.
+
+errdb_node() ->
+	SNode =
+	case net_kernel:longnames() of
+	true ->
+		"errdb@" ++ inet_db:gethostname() ++
+			 "." ++ inet_db:res_option(domain);
+	false ->
+		"errdb@" ++ inet_db:gethostname()
+	end,
+	list_to_atom(SNode).
+
--- src/errdb_web_web.erl
+++ src/errdb_web_web.erl
@@ -4,7 +4,9 @@
 %% @doc Web server for errdb_web.
 
 -module(errdb_web_web).
--author('author <author@example.com>').
+-author('author <ery.lee@gmail.com>').
+
+-import(errdb_web, [errdb_node/0]).
 
 -export([start/1, stop/0, loop/2]).
 
@@ -21,25 +23,56 @@ stop() ->
     mochiweb_http:stop(?MODULE).
 
 loop(Req, DocRoot) ->
-    "/" ++ Path = Req:get(path),
-    case Req:get(method) of
-        Method when Method =:= 'GET'; Method =:= 'HEAD' ->
-            case Path of
-                _ ->
-                    Req:serve_file(Path, DocRoot)
-            end;
-        'POST' ->
-            case Path of
-                _ ->
-                    Req:not_found()
-            end;
-        _ ->
-            Req:respond({501, [], []})
-    end.
+    Method = Req:get(method),
+	Path = list_to_tuple(string:tokens(Req:get(path), "/")),
+	handle(Method, Path, Req, DocRoot).
+
+handle('GET', {"rrdb", Key, "last"}, Req, _DocRoot) ->
+	case rpcall(errdb, last, [list_to_binary(Key)]) of
+    {ok, Fields, Record} -> 
+        Head = string:join(Fields, ","),
+        Line = line(Record),
+        Resp = list_to_binary(["time:", Head, "\n", Line]),
+        Req:ok({"text/plain", Resp});
+    {error, Reason} ->
+        Req:respond({500, [], atom_to_list(Reason)});
+    {badrpc, _Reason} ->
+        Req:respond({500, [], "failed to connect errdb"})
+	end;
+
+handle('GET', {"rrdb", Key, Range}, Req, _DocRoot) ->
+    [Begin,End|_] = string:tokens(Range, "-"),
+	case rpcall(errdb, fetch, [list_to_binary(Key), 
+        list_to_integer(Begin), list_to_integer(End)]) of
+    {ok, Fields, Records} -> 
+        Head = string:join(Fields, ","),
+        Lines = string:join([line(Record) || Record <- Records], "\n"),
+        Resp = list_to_binary(["time:", Head, "\n", Lines]),
+        Req:ok({"text/plain", Resp});
+    {error, Reason} ->
+        Req:respond({500, [], atom_to_list(Reason)});
+    {badrpc, _Reason} ->
+        Req:respond({500, [], "failed to connect errdb"})
+	end;
+
+handle('GET', PathTuple, Req, DocRoot) ->
+    Path = string:join(tuple_to_list(PathTuple), "/"),
+	io:format("Path: ~p~n", [Path]),
+    Req:serve_file(Path, DocRoot);
+
+handle(_, _, Req, _DocRoot) ->
+	Req:respond({501, [], <<"bad request">>}).
 
 %% Internal API
 
 get_option(Option, Options) ->
-    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
+    {proplists:get_value(Option, Options),
+	 proplists:delete(Option, Options)}.
 
+rpcall(Mod, Fun, Args) ->
+	rpc:call(errdb_node(), Mod, Fun, Args).
+
+line({Time, Values}) when is_integer(Time) and is_list(Values) ->
+    Line = string:join([extbif:to_list(V) || V <- Values], ","),
+    string:join([extbif:to_list(Time), Line], ":").
 
